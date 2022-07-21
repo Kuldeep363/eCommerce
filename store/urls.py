af92591afb0754308f5d076b1967c426f2ac2d18
unknown <kuldeep363rawat@gmail.com>
2022-07-21 14:51:38 +0530

    url resolved for product list api

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from . import views as vw
 
 urlpatterns = [
     path('', vw.home,name='home'),
-    path('api/aproducts/', vw.RetrieveProducts.as_view(),name='allProductDetail'),
+    path('api/products/', vw.RetrieveProducts.as_view(),name='allProductDetail'),
     path('api/product/<slug:slug>', vw.GetProductDetails.as_view(),name='productDetail'),
     path('api/add-product', vw.AddProduct.as_view(),name='addProduct'),
     path('api/update-product/<str:product_id>', vw.UpdateProduct.as_view(),name='updateProduct'),
